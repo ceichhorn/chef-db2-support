4170f3852e00f05d2c9d68cb36f9410fa8275977
William Spencer <wspencer@gannett.com>
2016-06-16 12:18:35 -0400

    started adding specs

--- spec/unit/recipes/mysql_spec.rb
+++ spec/unit/recipes/mysql_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'fauxhai'
 
-describe 'ruby-support::default' do
+describe 'ruby-support::mysql' do
   context 'On Centos 7.1 with defaults set' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'centos', version: '7.1.1503') do |node|
@@ -13,11 +13,13 @@ describe 'ruby-support::default' do
         node.default['ssh_keys'] = { test: ["test"] }
       end.converge(described_recipe)
     end
+
     # included recipes
-    recipe_list = ['gdp-base-linux']
+    recipe_list = ['ruby-support::default']
 
     recipe_list.each do |name|
       it 'includes the recipe ' + name do
+        stub_data_bag_item("commerce-solutions", "single-copy-self-service").and_return('scss')
         expect(chef_run).to include_recipe(name)
       end
     end
@@ -30,8 +32,8 @@ describe 'ruby-support::default' do
       expect(chef_run).to create_directory('/opt/rubyapp/test/config/')
     end
 
-    it 'creates the secrets template' do
-      expect(chef_run).to create_template('/opt/rubyapp/test/config/secrets.yml')
+    it 'creates the database template' do
+      expect(chef_run).to create_template('/opt/rubyapp/test/config/database.yml')
     end
 
     # installed packages
@@ -42,5 +44,6 @@ describe 'ruby-support::default' do
         expect(chef_run).to install_package(name)
       end
     end
-  end
+
+ end
 end
